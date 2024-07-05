042aa38181618a575ae191f2b24ea0b115f9282f
Иван Филатов <fivan999@yandex.ru>
2024-07-05 18:30:42 +0300

    async connection to db

--- src/db/base.py
+++ src/db/base.py
@@ -1,13 +1,14 @@
-from src.config import settings
+from config import settings
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio.session import async_sessionmaker
 
 
-engine = create_engine(
+engine = create_async_engine(
     url=settings.POSTGRES_URL,
     echo=True,
 )
 Base = declarative_base()
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
